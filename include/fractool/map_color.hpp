e537fd7321057a8bcc41c52af6e837a5b9941361
Andydevs <akanshul97@gmail.com>
2021-10-24 16:21:20 -0400

    Moved map_color code to separate source

---
+++ include/fractool/map_color.hpp
@@ -0,0 +1,9 @@
+#ifndef __FRACTOOL_MAPCOLOR_H__
+#define __FRACTOOL_MAPCOLOR_H__
+
+// External
+#include <opencv2/opencv.hpp>
+
+void map_color(unsigned size_x, unsigned size_y, uchar* param_buffer, uchar* color_buffer);
+
+#endif // __FRACTOOL_MAPCOLOR_H__
