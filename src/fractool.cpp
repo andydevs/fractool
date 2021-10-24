fb30f1f0e46a01c04cb5455b454bd4014eaa6feb
Andydevs <akanshul97@gmail.com>
2021-10-23 20:42:50 -0400

    Dynamically set color based on location

--- src/fractool.cpp
+++ src/fractool.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <Magick++.h>
 
+const char* size = "600x400";
+
 int main(int argc, char **argv) {
-    std::cout << "Making image!" << std::endl;
     Magick::InitializeMagick(*argv);
-    Magick::Image image("100x100", "blue");
-    image.write("blue_picture.png");
+
+    // Create image
+    Magick::Image image("100x100", "gray");
+
+    // Set image pixels
+    for (int i = 0; i < 100; ++i) {
+        for (int j = 0; j < 100; ++j) {
+            image.pixelColor(i, j, Magick::ColorGray((float)(i + j)/(100 + 100)));
+        }
+    }
+
+    // Write image
+    image.syncPixels();
+    image.write("fractal.png");
     return 0;
 }
