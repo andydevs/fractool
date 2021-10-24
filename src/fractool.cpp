6fc2463f3b6edda09575f4b6026613643222ea62
Andydevs <akanshul97@gmail.com>
2021-10-23 20:46:32 -0400

    Dynamically set pixel based on location

--- src/fractool.cpp
+++ src/fractool.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
 #include <Magick++.h>
 
+const int sizeX = 600;
+const int sizeY = 400;
 const char* size = "600x400";
+
+const int MAX_ITER = 256;
 
 int main(int argc, char **argv) {
     Magick::InitializeMagick(*argv);
 
     // Create image
-    Magick::Image image("100x100", "gray");
+    Magick::Image image(size, "gray");
 
     // Set image pixels
-    for (int i = 0; i < 100; ++i) {
-        for (int j = 0; j < 100; ++j) {
-            image.pixelColor(i, j, Magick::ColorGray((float)(i + j)/(100 + 100)));
+    bool escape;
+    for (int i = 0; i < sizeX; ++i) {
+        for (int j = 0; j < sizeY; ++j) {
+            escape = i + j > (sizeX + sizeY)/2;
+            image.pixelColor(i, j, Magick::ColorMono(escape));
         }
     }
 
