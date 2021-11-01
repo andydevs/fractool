8cc76278a5123c017888faaf194e4e1612954e46
Andydevs <akanshul97@gmail.com>
2021-10-31 21:16:27 -0400

    Create function for writing image

--- apps/fractool.cpp
+++ apps/fractool.cpp
@@ -8,6 +8,19 @@
 #include <opencv2/opencv.hpp>
 
 /**
+ * Write image to file
+ *
+ * @param size_x       horizontal image size
+ * @param size_y       vertical image size
+ * @param color_buffer color data buffer
+ * @param filename     file name to save to
+ */
+void write_image(const unsigned size_x, const unsigned size_y, unsigned char* color_buffer, const char* filename) {
+    cv::Mat image(size_y, size_x, CV_8UC3, color_buffer);
+    cv::imwrite(filename, image);
+}
+
+/**
  * Main program entrypoint
  */
 int main(int argc, char **argv) {
@@ -23,10 +36,7 @@ int main(int argc, char **argv) {
     // Run stages
     generate_mandelbrot(conf.image_size_x, conf.image_size_y, param_buffer);
     map_color(conf.image_size_x, conf.image_size_y, param_buffer, color_buffer);
-
-    // Save image
-    cv::Mat image(conf.image_size_y, conf.image_size_x, CV_8UC3, color_buffer);
-    cv::imwrite("fractal.png", image);
+    write_image(conf.image_size_x, conf.image_size_y, color_buffer, "fractal.png");
 
     // Delete resources
     delete[] param_buffer;
