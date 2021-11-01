a58df42a76083497cabf3efae0ea1b988509c3da
Andydevs <akanshul97@gmail.com>
2021-10-31 21:19:56 -0400

    Move write image to library

--- apps/fractool.cpp
+++ apps/fractool.cpp
@@ -1,24 +1,9 @@
 // Fractool
 #include <fractool/macros.hpp>
 #include <fractool/config.hpp>
-#include <fractool/map_color.hpp>
 #include <fractool/generate_mandelbrot.hpp>
-
-// External
-#include <opencv2/opencv.hpp>
-
-/**
- * Write image to file
- *
- * @param size_x       horizontal image size
- * @param size_y       vertical image size
- * @param color_buffer color data buffer
- * @param filename     file name to save to
- */
-void write_image(const unsigned size_x, const unsigned size_y, unsigned char* color_buffer, const char* filename) {
-    cv::Mat image(size_y, size_x, CV_8UC3, color_buffer);
-    cv::imwrite(filename, image);
-}
+#include <fractool/map_color.hpp>
+#include <fractool/write_image.hpp>
 
 /**
  * Main program entrypoint
