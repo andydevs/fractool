be4d0c1e13bf3859e2056af58ad175e497ae5c03
Andydevs <akanshul97@gmail.com>
2021-12-09 13:45:43 -0500

    Use pytest instead of unittest

--- scripts/integration_test/run.py
+++ scripts/integration_test/run.py
@@ -1,49 +1,45 @@
 #!/usr/bin/env python3
-import unittest
+import pytest
 from PIL import Image
 import subprocess as sp
 import numpy as np
 import os
 
+expected_rel = 'expected'
+executable_rel = '../../build/out/fractool/fractool'
 
-class CLIIntegrationTestCase(unittest.TestCase):
+
+@pytest.fixture
+def tmp_env(tmp_path):
     """
-    Test fractool cli under various conditions
+    Create temporary environment for each integration tests
     """
-    envdir = 'tmp'
-    expected = '../expected'
-    executable = '../../../build/out/fractool/fractool'
+    curr = os.getcwd()
+    executable = os.path.join(curr, executable_rel)
+    expected = os.path.join(curr, expected_rel)
+    os.chdir(tmp_path)
+    yield executable, expected
+    os.chdir(curr)
 
-    @classmethod
-    def setUpClass(cls):
-        if not os.path.exists(cls.envdir): 
-            os.mkdir(cls.envdir)
-        os.chdir(cls.envdir)
-        super().setUpClass()
 
-    def test_no_options(self):
-        self.expected_file = 'no-options.png'
-        self.args = []
-        self.result = sp.run([self.executable] + self.args, capture_output=True)
-        self.assertEqual(self.result.returncode, 0)
-        with Image.open('fractal.png') as actual, \
-            Image.open(f'{self.expected}/{self.expected_file}') as expected:
-            aAct = np.array(actual)
-            aExp = np.array(expected)
-            np.testing.assert_array_equal(aAct, aExp)
-
-    def test_algorithm_julia(self):
-        self.expected_file = 'algorithm-julia.png'
-        self.args = ['--algorithm', 'julia']
-        self.result = sp.run([self.executable] + self.args, capture_output=True)
-        self.assertEqual(self.result.returncode, 0)
-        with Image.open('fractal.png') as actual, \
-            Image.open(f'{self.expected}/{self.expected_file}') as expected:
-            aAct = np.array(actual)
-            aExp = np.array(expected)
-            np.testing.assert_array_equal(aAct, aExp)
+@pytest.mark.parametrize('expected_file,args', [
+    ('no-options.png',      []),
+    ('algorithm-julia.png', ['--algorithm', 'julia'])
+])
+def test_cli(tmp_env,expected_file,args):
+    """
+    Test command line tool with various option combinations
+    """
+    executable, expected = tmp_env
+    result = sp.run([executable] + args, capture_output=True)
+    assert result.returncode == 0
+    with Image.open('fractal.png') as actual, \
+        Image.open(os.path.join(expected, expected_file)) as expected:
+        aAct = np.array(actual)
+        aExp = np.array(expected)
+        np.testing.assert_array_equal(aAct, aExp)
 
 
 # Run unittests
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    pytest.main(['--verbose', 'run.py'])
