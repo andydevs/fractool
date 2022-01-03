624d8d5adedd36f151069f036df55ee01f1b176e
Andydevs <akanshul97@gmail.com>
2022-01-02 22:01:39 -0500

    Moved Boost import and OpenCV import to separate modules

---
+++ cmake/ImportBoost.cmake
@@ -0,0 +1,39 @@
+# External Project
+include(ExternalProject)
+
+# Configure boost
+ExternalProject_Add(
+    boost
+    URL "https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.gz"
+    CONFIGURE_COMMAND <SOURCE_DIR>/bootstrap.sh
+    BUILD_COMMAND <SOURCE_DIR>/b2
+    INSTALL_COMMAND <SOURCE_DIR>/b2 install --prefix=<INSTALL_DIR>
+    BUILD_IN_SOURCE 1
+)
+ExternalProject_Get_Property(boost install_dir)
+add_definitions(-DBOOST_LOG_DYN_LINK)
+include_directories(${install_dir}/include)
+
+# Log Setup library
+add_library(boost::log_setup SHARED IMPORTED)
+set_property(
+    TARGET boost::log_setup 
+    PROPERTY IMPORTED_LOCATION 
+        ${install_dir}/lib/libboost_log_setup${CMAKE_SHARED_LIBRARY_SUFFIX})
+add_dependencies(boost::log_setup boost)
+
+# Log library
+add_library(boost::log SHARED IMPORTED)
+set_property(
+    TARGET boost::log 
+    PROPERTY IMPORTED_LOCATION 
+        ${install_dir}/lib/libboost_log${CMAKE_SHARED_LIBRARY_SUFFIX})
+add_dependencies(boost::log boost)
+
+# Program Options library
+add_library(boost::program_options SHARED IMPORTED)
+set_property(
+    TARGET boost::program_options 
+    PROPERTY IMPORTED_LOCATION 
+        ${install_dir}/lib/libboost_program_options${CMAKE_SHARED_LIBRARY_SUFFIX})
+add_dependencies(boost::program_options boost)
---
+++ cmake/ImportOpenCV.cmake
@@ -0,0 +1,30 @@
+# External Project
+include(ExternalProject)
+
+# Configure opencv
+ExternalProject_Add(
+    opencv
+    GIT_REPOSITORY "https://github.com/opencv/opencv.git"
+    GIT_TAG "4.5.5"
+    CMAKE_ARGS
+        -DOPENCV_FORCE_3RDPARTY_BUILD=ON
+        -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
+)
+ExternalProject_Get_Property(opencv install_dir)
+include_directories(${install_dir}/include/opencv4)
+
+# Core library
+add_library(opencv::core SHARED IMPORTED)
+set_property(
+    TARGET opencv::core 
+    PROPERTY IMPORTED_LOCATION 
+        ${install_dir}/lib/libopencv_core${CMAKE_SHARED_LIBRARY_SUFFIX})
+add_dependencies(opencv::core opencv)
+
+# Image Codecs library
+add_library(opencv::imgcodecs SHARED IMPORTED)
+set_property(
+    TARGET opencv::imgcodecs 
+    PROPERTY IMPORTED_LOCATION 
+        ${install_dir}/lib/libopencv_imgcodecs${CMAKE_SHARED_LIBRARY_SUFFIX})
+add_dependencies(opencv::imgcodecs opencv)
