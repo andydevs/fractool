fc6235e3e0ddd29110f590baed53ea62ea27985e
Andydevs <akanshul97@gmail.com>
2022-01-03 18:56:00 -0500

    Add both libs and lib links in linux

--- cmake/ImportBoost.cmake
+++ cmake/ImportBoost.cmake
@@ -39,5 +39,6 @@ set_property(
 add_dependencies(boost::program_options boost)
 
 # Install libraries with binary
-file(GLOB boost_libs ${install_dir}/lib/libboost_*${CMAKE_SHARED_LIBRARY_SUFFIX})
-install(FILES ${boost_libs} DESTINATION lib)
+file(GLOB boost_lib_links ${install_dir}/lib/libboost_*${CMAKE_SHARED_LIBRARY_SUFFIX})
+file(GLOB boost_libs ${install_dir}/lib/libboost_*${CMAKE_SHARED_LIBRARY_SUFFIX}.*.*.*)
+install(FILES ${boost_libs} ${boost_lib_links} DESTINATION lib)
