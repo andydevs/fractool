9c453fb74e60a07130cbcad735d9d335471c9be4
Andydevs <akanshul97@gmail.com>
2022-02-09 23:39:39 -0500

    Using specific versioned libraries... accounting for different oss

--- cmake/ImportBoost.cmake
+++ cmake/ImportBoost.cmake
@@ -14,29 +14,27 @@ ExternalProject_Get_Property(boost install_dir)
 add_definitions(-DBOOST_LOG_DYN_LINK)
 include_directories(${install_dir}/include)
 
+# ADD VERSION NUMBER IF WE'RE ON LINUX AAAAA!!!!!!!!!!!!
+if(${CMAKE_SHARED_LIBRARY_SUFFIX} EQUAL ".so")
+    set(SUFF ${CMAKE_SHARED_LIBRARY_SUFFIX}.1.78.0)
+else()
+    set(SUFF ${CMAKE_SHARED_LIBRARY_SUFFIX})
+endif()
+
 # Log Setup library
 add_library(boost::log_setup SHARED IMPORTED)
-set_property(
-    TARGET boost::log_setup 
-    PROPERTY IMPORTED_LOCATION 
-        ${install_dir}/lib/libboost_log_setup${CMAKE_SHARED_LIBRARY_SUFFIX})
+set_property(TARGET boost::log_setup PROPERTY IMPORTED_LOCATION ${install_dir}/lib/libboost_log_setup${SUFF})
 add_dependencies(boost::log_setup boost)
-install(IMPORTED_RUNTIME_ARTIFACTS boost::log_setup LIBRARY DESTINATION lib)
+install(IMPORTED_RUNTIME_ARTIFACTS boost::log_setup RUNTIME DESTINATION lib)
 
 # Log library
 add_library(boost::log SHARED IMPORTED)
-set_property(
-    TARGET boost::log 
-    PROPERTY IMPORTED_LOCATION 
-        ${install_dir}/lib/libboost_log${CMAKE_SHARED_LIBRARY_SUFFIX})
+set_property(TARGET boost::log PROPERTY IMPORTED_LOCATION ${install_dir}/lib/libboost_log${SUFF})
 add_dependencies(boost::log boost)
-install(IMPORTED_RUNTIME_ARTIFACTS boost::log LIBRARY DESTINATION lib)
+install(IMPORTED_RUNTIME_ARTIFACTS boost::log RUNTIME DESTINATION lib)
 
 # Program Options library
 add_library(boost::program_options SHARED IMPORTED)
-set_property(
-    TARGET boost::program_options 
-    PROPERTY IMPORTED_LOCATION 
-        ${install_dir}/lib/libboost_program_options${CMAKE_SHARED_LIBRARY_SUFFIX})
+set_property(TARGET boost::program_options PROPERTY IMPORTED_LOCATION ${install_dir}/lib/libboost_program_options${SUFF})
 add_dependencies(boost::program_options boost)
-install(IMPORTED_RUNTIME_ARTIFACTS boost::program_options LIBRARY DESTINATION lib)
+install(IMPORTED_RUNTIME_ARTIFACTS boost::program_options RUNTIME DESTINATION lib)
--- cmake/ImportOpenCV.cmake
+++ cmake/ImportOpenCV.cmake
@@ -14,20 +14,24 @@ ExternalProject_Add(
 ExternalProject_Get_Property(opencv install_dir)
 include_directories(${install_dir}/include/opencv4)
 
+# ADD VERSION NUMBER DIFFERENTLY IF WE'RE ON WINDOWS OMGOMGOMGOMGOMGOMGOMGOMGOMG!!!!!
+# ALSO DYNAMIC LIBS ARE IN DIFFERENT LOCATION THAN STATIC LIBS OOOOOOOOOO!!!!!
+if (WIN32)
+    set(SUFF 455${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(LDIR bin)
+else()
+    set(SUFF .4.5.5${CMAKE_SHARED_LIBRARY_SUFFIX})
+    set(LDIR lib)
+endif()
+
 # Core library
 add_library(opencv::core SHARED IMPORTED)
-set_property(
-    TARGET opencv::core 
-    PROPERTY IMPORTED_LOCATION 
-        ${install_dir}/lib/libopencv_core${CMAKE_SHARED_LIBRARY_SUFFIX})
+set_property(TARGET opencv::core PROPERTY IMPORTED_LOCATION ${install_dir}/${LDIR}/libopencv_core${SUFF})
 add_dependencies(opencv::core opencv)
-install(IMPORTED_RUNTIME_ARTIFACTS opencv::core LIBRARY DESTINATION lib)
+install(IMPORTED_RUNTIME_ARTIFACTS opencv::core RUNTIME DESTINATION ${LDIR})
 
 # Image Codecs library
 add_library(opencv::imgcodecs SHARED IMPORTED)
-set_property(
-    TARGET opencv::imgcodecs 
-    PROPERTY IMPORTED_LOCATION 
-        ${install_dir}/lib/libopencv_imgcodecs${CMAKE_SHARED_LIBRARY_SUFFIX})
+set_property(TARGET opencv::imgcodecs PROPERTY IMPORTED_LOCATION ${install_dir}/${LDIR}/libopencv_imgcodecs${SUFF})
 add_dependencies(opencv::imgcodecs opencv)
-install(IMPORTED_RUNTIME_ARTIFACTS opencv::imgcodecs LIBRARY DESTINATION lib)
+install(IMPORTED_RUNTIME_ARTIFACTS opencv::imgcodecs RUNTIME DESTINATION ${LDIR})
