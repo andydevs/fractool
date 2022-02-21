ca4778042b45dd40652a34c0b91b97b34e51eb95
Andydevs <akanshul97@gmail.com>
2022-02-20 21:12:13 -0500

    Create macro for boost libraries and add chrono library

--- cmake/ImportBoost.cmake
+++ cmake/ImportBoost.cmake
@@ -22,56 +22,22 @@ else()
     set(SUFF ${CMAKE_SHARED_LIBRARY_SUFFIX})
 endif()
 
-# Log Setup library
-add_library(boost::log_setup SHARED IMPORTED)
-set_property(TARGET boost::log_setup 
-             PROPERTY IMPORTED_LOCATION 
-                ${install_dir}/lib/libboost_log_setup${SUFF})
-add_dependencies(boost::log_setup boost)
-install(IMPORTED_RUNTIME_ARTIFACTS boost::log_setup 
-        RUNTIME DESTINATION lib)
+# Setup library macro
+macro(import_boost_library libname)
+    add_library(boost::${libname} SHARED IMPORTED)
+    set_property(TARGET boost::${libname}
+                 PROPERTY IMPORTED_LOCATION
+                    ${install_dir}/lib/libboost_${libname}${SUFF})
+    add_dependencies(boost::${libname} boost)
+    install(IMPORTED_RUNTIME_ARTIFACTS boost::${libname}
+            RUNTIME DESTINATION lib)
+endmacro()
 
-# Log library
-add_library(boost::log SHARED IMPORTED)
-set_property(TARGET boost::log 
-             PROPERTY IMPORTED_LOCATION 
-                ${install_dir}/lib/libboost_log${SUFF})
-add_dependencies(boost::log boost)
-install(IMPORTED_RUNTIME_ARTIFACTS boost::log 
-        RUNTIME DESTINATION lib)
-
-# Program Options library
-add_library(boost::program_options SHARED IMPORTED)
-set_property(TARGET boost::program_options 
-             PROPERTY IMPORTED_LOCATION 
-                ${install_dir}/lib/libboost_program_options${SUFF})
-add_dependencies(boost::program_options boost)
-install(IMPORTED_RUNTIME_ARTIFACTS boost::program_options 
-        RUNTIME DESTINATION lib)
-
-# Thread library
-add_library(boost::thread SHARED IMPORTED)
-set_property(TARGET boost::thread 
-             PROPERTY IMPORTED_LOCATION 
-                ${install_dir}/lib/libboost_thread${SUFF})
-add_dependencies(boost::thread boost)
-install(IMPORTED_RUNTIME_ARTIFACTS boost::thread 
-        RUNTIME DESTINATION lib)
-
-# Filesystem library
-add_library(boost::filesystem SHARED IMPORTED)
-set_property(TARGET boost::filesystem 
-             PROPERTY IMPORTED_LOCATION 
-                ${install_dir}/lib/libboost_filesystem${SUFF})
-add_dependencies(boost::filesystem boost)
-install(IMPORTED_RUNTIME_ARTIFACTS boost::filesystem 
-        RUNTIME DESTINATION lib)
-
-# Regex library
-add_library(boost::regex SHARED IMPORTED)
-set_property(TARGET boost::regex 
-             PROPERTY IMPORTED_LOCATION 
-                ${install_dir}/lib/libboost_regex${SUFF})
-add_dependencies(boost::regex boost)
-install(IMPORTED_RUNTIME_ARTIFACTS boost::regex 
-        RUNTIME DESTINATION lib)
+# Import libraries
+import_boost_library(log_setup)
+import_boost_library(log)
+import_boost_library(program_options)
+import_boost_library(thread)
+import_boost_library(filesystem)
+import_boost_library(regex)
+import_boost_library(chrono)
