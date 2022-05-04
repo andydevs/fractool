6e06d7bbab9d70d0e848396814eca60ad9e2addb
Andydevs <akanshul97@gmail.com>
2022-05-03 22:49:40 -0400

    add extra command alias to dev.sh

--- env/dev.sh
+++ env/dev.sh
@@ -1,4 +1,6 @@
 ROOT_PATH=$(pwd)
 export PATH="$PATH:$ROOT_PATH/build/test-install/bin"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$ROOT_PATH/build/test-install/lib"
-alias configure="cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$ROOT_PATH/build/test-install"
+alias configure="cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$ROOT_PATH/build/test-install"
+alias build="cmake --build build"
+alias install="cmake --install build"
