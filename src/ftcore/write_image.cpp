cc48325b7856a5425199e001d7480ec58055ef83
Andydevs <akanshul97@gmail.com>
2022-05-17 22:09:33 -0400

    Include libpng in code

--- src/ftcore/write_image.cpp
+++ src/ftcore/write_image.cpp
@@ -1,7 +1,7 @@
 #include <fractool/ftcore/write_image.hpp>
 
 // External
-#include <opencv2/opencv.hpp>
+#include <png.h>
 #include <boost/log/trivial.hpp>
 
 
@@ -15,7 +15,4 @@
  */
 void write_image(const unsigned size_x, const unsigned size_y, unsigned char* color_buffer, const char* filename) {
     BOOST_LOG_TRIVIAL(info) << "Writing Image...";
-    
-    cv::Mat image(size_y, size_x, CV_8UC3, color_buffer);
-    cv::imwrite(filename, image);
 }
