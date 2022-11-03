58f5407599682c802523d1379ec6719a2b2a2fb0
Andydevs <akanshul97@gmail.com>
2022-11-03 15:15:26 -0400

    Add DragNDrop generator for mac

--- env/dev.sh
+++ env/dev.sh
@@ -2,6 +2,6 @@ export ROOT_DIR=$(pwd)
 export PATH="$PATH:$ROOT_DIR/build/test-install/bin"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$ROOT_DIR/build/test-install/lib"
 export BUILD_TYPE="Debug"
-alias configure="cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/build/test-install -DCPACK_PACKAGE_FILE_NAME=FracTool-Debug-$OSTYPE"
+alias configure="cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$ROOT_DIR/build/test-install"
 alias build="cmake --build build"
 alias install="cmake --install build"
